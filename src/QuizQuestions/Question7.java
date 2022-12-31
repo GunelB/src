324f92713b1a58f30be2ae18cf1ac5343ce08b79
Hayri Aslan <hayriaslan88@gmail.com>
2022-12-30 21:49:00 -0500

    quiz

---
+++ src/QuizQuestions/Question7.java
@@ -0,0 +1,14 @@
+package QuizQuestions;
+
+public class Question7 {
+    public static void main(String[] args) {
+        int a =5;
+        int b =6;
+        System.out.println(a+b*2);
+        // a+b*2       /* -> +-
+        // a + 6*2
+        // a + 12
+        // 5 + 12
+        // 17
+    }
+}
