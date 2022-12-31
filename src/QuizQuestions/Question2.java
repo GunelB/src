839cd90d23f22fbfbbfd154f81f1b41c5745b7c3
Hayri Aslan <hayriaslan88@gmail.com>
2022-12-30 21:14:55 -0500

    quiz

--- src/QuizQuestions/Question2.java
+++ src/QuizQuestions/Question2.java
@@ -6,7 +6,7 @@ public class Question2 {
         int b = 22;
 
         int c = a + b + a++ + b++ + ++a + ++b;
-            //a + a++ + ++a;
+        // a + a++ + ++a;
         // 11 + a++ + ++a;   a=11;
         // 11 + 11 + ++a;    a=12;
         // 11 + 11 + 13;     a=13;
