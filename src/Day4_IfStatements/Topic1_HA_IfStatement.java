1984773130ee4b01ea794351ddfd5f212af02d10
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-03 19:13:29 -0500

    Day4

---
+++ src/Day4_IfStatements/Topic1_HA_IfStatement.java
@@ -0,0 +1,23 @@
+package Day4_IfStatements;
+
+public class Topic1_HA_IfStatement {
+    public static void main(String[] args) {
+        System.out.println("I want to print this line");
+
+        //System.out.println("I don't want to print this line");
+        int a = 10;
+        /*
+        if (condition){
+            boolean   true, false
+           if condition is true, then execute this block
+           there might be another line here
+        }
+         */
+        if (a == 10){
+            System.out.println("I want to print this line if a=10");
+        }
+
+
+        System.out.println("End of the code");
+    }
+}
