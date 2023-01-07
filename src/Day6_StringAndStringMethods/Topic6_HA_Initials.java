cd72ff7d49ba4afb4a7226e4b45cfca36309374b
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-06 21:16:02 -0500

    Day6

---
+++ src/Day6_StringAndStringMethods/Topic6_HA_Initials.java
@@ -0,0 +1,25 @@
+package Day6_StringAndStringMethods;
+
+import java.util.Scanner;
+
+public class Topic6_HA_Initials {
+    public static void main(String[] args) {
+        //ObjectType VariableName = new ObjectType(parameter);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String Name = scan.nextLine();
+        System.out.println("Please enter your surname");
+        String Surname = scan.nextLine();
+        // Hayri Aslan
+        // H. A.
+
+        // Elizabeth Rebecca
+        // E. R.
+
+        // Name = Hayri
+        // Surname = Aslan
+        String Initials = Name.charAt(0)+". "+Surname.charAt(0)+".";
+                // H. A.
+        System.out.println("Initials = " + Initials);
+    }
+}
