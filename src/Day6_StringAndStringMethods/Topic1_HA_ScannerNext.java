a35899ef79d81ec1339f7f304e990c06e98d1e4e
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-06 19:23:04 -0500

    Day6

---
+++ src/Day6_StringAndStringMethods/Topic1_HA_ScannerNext.java
@@ -0,0 +1,35 @@
+package Day6_StringAndStringMethods;
+
+import java.util.Scanner;
+
+public class Topic1_HA_ScannerNext {
+    public static void main(String[] args) {
+        // nextLine() to get string
+        // next() will read your input until it sees a space " "
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter 2 veggies and separate them with space");
+        String Veggie1 = scan.next();
+        String Veggie2 = scan.next();
+
+        System.out.println("Veggie1 = " + Veggie1);
+        System.out.println("Veggie2 = " + Veggie2);
+
+        System.out.println("Please enter your name, surname and age and separate them with space");
+        // Hayri Aslan 34
+        String Name = scan.next();
+        String Surname = scan.next();
+        int Age = scan.nextInt();
+
+        System.out.println("Name = " + Name);
+        System.out.println("Surname = " + Surname);
+        System.out.println("Age = " + Age);
+
+        System.out.printf("Name = %s, Surname = %s, Age = %d", Name, Surname, Age);
+
+        /*
+        %s  <- string
+        %d  <- numeric
+         */
+
+    }
+}
