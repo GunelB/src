f07ad7cfdb8e01b86ef0fae38bac4b77878cd8c9
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-06 20:13:09 -0500

    Day6

---
+++ src/Day6_StringAndStringMethods/Topic3_JVM_JRE_JDK.java
@@ -0,0 +1,19 @@
+package Day6_StringAndStringMethods;
+
+public class Topic3_JVM_JRE_JDK {
+    /*
+    JVM <- Java Virtual Machine
+    The Java Virtual Machine (JVM) is the virtual machine that runs the Java bytecodes. The JVM doesn't understand Java source code; that's why you need compile your *.java files to obtain *.class files that contain the bytecodes understood by the JVM.
+
+    JVM is the Java Virtual Machine – it actually runs Java ByteCode.
+
+    JRE <- Java Runtime Environment
+
+    The Java Runtime Environment (JRE) provides the libraries, the Java Virtual Machine, and other components to run applets and applications written in the Java programming language.
+
+    JRE is the Java Runtime Environment – it contains a JVM, among other things, and is what you need to run a Java program.
+
+    JDK <- Java Development Kit
+    The JDK is a superset of the JRE, and contains everything that is in the JRE, plus tools such as the compilers and debuggers necessary for developing applets and applications.
+     */
+}
