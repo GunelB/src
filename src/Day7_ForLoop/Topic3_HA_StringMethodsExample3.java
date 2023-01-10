10c5a1f398637dbd80df735e54e25b2904bbfc64
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-09 20:44:00 -0500

    Day7

---
+++ src/Day7_ForLoop/Topic3_HA_StringMethodsExample3.java
@@ -0,0 +1,23 @@
+package Day7_ForLoop;
+
+public class Topic3_HA_StringMethodsExample3 {
+    public static void main(String[] args) {
+        /*
+        abcdefghijklmnopqrstuvwxy
+        divide this word to 5 and print it
+        abcde
+        fghij
+        klmno
+        pqrst
+        uvwxy
+         */
+        String Word = "abcdefghijklmnopqrstuvwxy";
+        System.out.println(Word.substring(0,5));
+        System.out.println(Word.substring(5,10));
+        System.out.println(Word.substring(10,15));
+        System.out.println(Word.substring(15,20));
+
+        System.out.println(Word.substring(20,25));
+        System.out.println(Word.substring(20));  // <- same with above
+    }
+}
