00ae6431250b5760fc93ca3e23df95f694a316ff
GunelB <gunel.veli77@gmail.com>
2023-01-11 15:20:36 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day8_ForLoop/Topic10_GB_Example10.java
@@ -0,0 +1,15 @@
+package Day8_ForLoop;
+
+public class Topic10_GB_Example10 {
+    public static void main(String[] args) {
+        for (int i = 15; i > 0; i--) {
+            System.out.print(i + " ");
+            if (i == 4) {
+                break;
+
+            }
+        }
+    }
+}
+
+
