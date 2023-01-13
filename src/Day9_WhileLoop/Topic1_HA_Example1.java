458ccab8474699e75edda70ce380c06dd4b52479
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-12 19:22:19 -0500

    Day9

---
+++ src/Day9_WhileLoop/Topic1_HA_Example1.java
@@ -0,0 +1,42 @@
+package Day9_WhileLoop;
+
+import java.util.Scanner;
+
+public class Topic1_HA_Example1 {
+    public static void main(String[] args) {
+        /*
+        Get 2 numbers from user
+        count how many numbers we have using for loop
+        count the numbers that can divide by 3
+         */
+        // create scanner
+        // inform user for the first number
+        // assign it to Start
+        // inform user for the second number
+        // assign it to End
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter first number");
+        int Start = scan.nextInt();
+        System.out.println("Please enter second number");
+        int End = scan.nextInt();
+
+        /*
+        for(int i = starting; condition(endpoint); iteration(increase or decrease)){
+            code block
+        }
+         */
+        int Count = 0;
+        for (int i = Start; i <= End ; i++) {
+            // count the numbers
+            //int Count = 0;  //<- this will set the count to zero everytime code get executed
+            if (i % 3 == 0){
+                System.out.println(i+" can be divided by 3");
+                Count = Count + 1;
+            }
+
+        }
+        System.out.println("Count = "+Count);
+
+        // 6,9
+    }
+}
