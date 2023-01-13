de428fd3d9e2b7ec372e1c94615f3df2b7c987fe
GunelB <gunel.veli77@gmail.com>
2023-01-12 19:23:08 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Homeworks/Homework_2.java
@@ -0,0 +1,25 @@
+package Homeworks;
+
+import java.io.StringReader;
+import java.util.Scanner;
+
+public class Homework_2 {
+
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the word mum");
+
+        boolean IsItPalindrome = true;
+
+            System.out.println("It is palindrome ");
+            System.out.println("Is not palindrome");
+        }
+        //int number = scan.nextInt();
+        //int Sum = scan.nextInt();
+        //int Remainder = scan.nextInt();
+        //int sum = 0;
+        //for (int i = 0; i < ; i++) {
+
+        }
+
+
