852b0212c6b48905ed255b09b34921bdfd715f55
GunelB <gunel.veli77@gmail.com>
2023-01-17 22:06:10 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day11_Recap/Topic7_GB.java
@@ -0,0 +1,15 @@
+package Day11_Recap;
+
+import java.util.Scanner;
+
+public class Topic7_GB {
+    public static void main(String[] args) {
+/*
+Write   a   program   that   will   count   how   many   times   "j"   is   found   in   any   given   String:
+String mystring="java is fun, but sometimes java is just pain.Love java java java";
+
+ */
+        String myString="java is fun, but sometimes java is just pain.Love java java java";
+
+    }
+}
---
+++ src/Day11_Recap/Topic8_GB.java
@@ -0,0 +1,7 @@
+package Day11_Recap;
+
+public class Topic8_GB {
+    public static void main(String[] args) {
+
+    }
+}
