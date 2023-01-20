95fdc25fe3fabba3c881302e01e7b0213ad99438
GunelB <gunel.veli77@gmail.com>
2023-01-19 19:45:08 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Homeworks/Homework_16.java
@@ -0,0 +1,43 @@
+package Homeworks;
+
+import java.util.Scanner;
+
+public class Homework_16 {
+    public static void main(String[] args) {
+
+        //  task8
+        //  Given a string, return a new string where the first and last chars have been exchanged.
+//   frontBack("code") → "eodc"
+        //  frontBack("a") → "a"
+        //  frontBack("ab") → "ba"
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a new word");
+        String word = scan.next();
+        String replace ="";
+
+        for (int i = word.length() - 1; i <= 0; i--) {
+
+        }
+        if (word.length()==0) {
+            char start= word.charAt(0);
+            char end = word.charAt(word.length()-1);
+
+        }
+
+
+            //  System.out.println("Enter a word");
+            //   String word = scan.next();
+            //  String reverse = "";
+
+            //    for (int i = word.length() - 1; i>= 0; i--) {
+        }       //       reverse = reverse + word.charAt(i);
+        }
+        //   if (reverse.equals(word)) {
+        //       System.out.println("palindrome");
+        //    } else {
+        //       System.out.println("not palindrome");
+
+
+
+   // }
+// }
