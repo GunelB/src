eee8cd4258ceaa1d63c9da758e5889f83a4d8f40
GunelB <gunel.veli77@gmail.com>
2023-01-20 20:01:08 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day13_Arrays/Topic2_GB_Arrays.java
@@ -0,0 +1,24 @@
+package Day13_Arrays;
+
+public class Topic2_GB_Arrays {
+    public static void main(String[] args) {
+        int[]MyIntArray ={3,5,9,7};
+        System.out.println("The first value in the array="+MyIntArray[0]);
+        System.out.println("The second value="+MyIntArray[1]);
+        String[] Names ={"adam","Emma","Roma","Aden","Oznur","Tesla","Nurat"};
+        for (int i = 0; i <= Names.length-1; i++) {
+            System.out.println(Names[i]);
+
+        }
+      //  System.out.println("The first name in the list ="+Names[3]);
+        //System.out.println("The first name in the list ="+Names[2]);
+        //System.out.println("The length of the Name is "+ Names.length);
+        //System.out.println("The length of the Name is " + Names[Names.length-1]);
+        //int[] MyNumber = {5,7,8,9};
+        //System.out.println(MyNumber[MyNumber.length-1]);
+        //for (int i= 0; i < MyNumber.length-1; i++) {
+         //   System.out.println(MyNumber[i]);
+
+        }
+
+ }
