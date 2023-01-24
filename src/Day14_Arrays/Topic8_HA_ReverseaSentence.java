4475cf13d4782935081924b61663474274dbc1f8
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-23 21:46:43 -0500

    Day14

---
+++ src/Day14_Arrays/Topic8_HA_ReverseaSentence.java
@@ -0,0 +1,20 @@
+package Day14_Arrays;
+
+public class Topic8_HA_ReverseaSentence {
+    public static void main(String[] args) {
+        String Sentence = "I love Java";
+        // Java love I
+
+        String [] MySentenceArray = Sentence.split(" ");
+        //{"I", "love", "Java"}
+        //  0      1       2
+        // go from 2 to 0
+        // MySentenceArray.length = 3
+
+        for (int i = MySentenceArray.length - 1; i >=0; i--) {
+            // i = 2
+            System.out.print(MySentenceArray[i]+" ");
+        }
+
+    }
+}
