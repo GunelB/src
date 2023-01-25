378de6a799c3726a0443ef8e227c9407dd5cf2a1
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-24 21:56:59 -0500

    Day15

---
+++ src/Day15_ArraysandForEach/Topic4_HA_MultiDimensionalArrays.java
@@ -0,0 +1,16 @@
+package Day15_ArraysandForEach;
+
+public class Topic4_HA_MultiDimensionalArrays {
+    public static void main(String[] args) {
+        int [] Numbers = {55,99,33,64};
+        // this is 1-D array
+
+        //2-D array
+        int [][] Number2D = { {55,99,33,64} , {78,89,23,11}   };
+        System.out.println( Number2D[1][3] );
+
+        // String 3-D Array
+        // String [][][] String3D = {{{"Array1-item1", "Array1-item2"}, {"Array1-item1", "Array1-item2"}}, {{"Array2-item1", "Array2-item2"}}, {{"Array3-item1", "Array3-item2"}}};
+
+    }
+}
