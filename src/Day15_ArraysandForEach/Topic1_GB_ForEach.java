aa9d6072724cba1e4bdd62e7dbe4e6bb8f4ee19d
GunelB <gunel.veli77@gmail.com>
2023-01-24 19:53:25 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

--- src/Day15_ArraysandForEach/Topic1_GB_ForEach.java
+++ src/Day15_ArraysandForEach/Topic1_GB_ForEach.java
@@ -1,11 +1,13 @@
 package Day15_ArraysandForEach;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Topic1_GB_ForEach {
     public static void main(String[] args) {
         String Hi= "Hello";
         char [] HiArray =Hi.toCharArray();
+        System.out.println(Arrays.toString(HiArray));
         for (char Greetings:HiArray) {
             System.out.println("Greetings = " + Greetings);
             
---
+++ src/Day15_ArraysandForEach/Topic2_GB_Example.java
@@ -0,0 +1,20 @@
+package Day15_ArraysandForEach;
+
+public class Topic2_GB_Example {
+    public static void main(String[] args) {
+        //Given an array of ints length 3, return a new array with the elements in reverse order, so //{1, 2, 3} becomes {3, 2, 1}.
+        //I wiil make array
+        //I need empty array
+        //the first array iterate -downwards
+
+        int[] Array1 ={1,2,3};
+        //need empty array
+        //datatype[]Variable = new Datatype[length]
+
+        int[]Array2=new int[Array1.length];          //equal array3
+        //need to iterate downwards
+        for (int i = Array1.length-1; i >=0 ; i--) {
+
+        }
+    }
+}
