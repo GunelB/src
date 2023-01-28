bd175e84ce235e8af472df92a7a4142837d25ae3
GunelB <gunel.veli77@gmail.com>
2023-01-27 21:59:22 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

--- src/Day17_Methods/Topic2_GB_MethodsCont.java
+++ src/Day17_Methods/Topic2_GB_MethodsCont.java
@@ -2,9 +2,8 @@ package Day17_Methods;
 
 public class Topic2_GB_MethodsCont {
     public static void main(String[] args) {
-   //  Food("Tomato","SeaSalt","Olive");
-      PrintSum(7,9);
-     return;
+   //Food("Tomato","SeaSalt","Olive");
+   System.out.println(Sum(4,5));
     }
 
     public static void Food(String Veggies,String Salt,String Oil){
@@ -13,7 +12,10 @@ public class Topic2_GB_MethodsCont {
 
         System.out.println("VeggiesSaltAndOil = " + VeggiesSaltAndOil);
     }
-    public static int Sum(int num1,int num2){       //create a method public static returntypemethodName
+    public static int Sum(int num1,int num2){
+        //create a method public static returntypemethodName
+        int Sum;
         return num1+num2;
     }
+      
 }
--- src/Day17_Methods/Topic3_GB_Example1.java
+++ src/Day17_Methods/Topic3_GB_Example1.java
@@ -2,24 +2,24 @@ package Day17_Methods;
 
 import java.util.Scanner;
 
-public class Topic3_GB_Exaqmple1 {
+public class Topic3_GB_Example1 {
     public static void main(String[] args) {
         Scanner scan =new Scanner(System.in);
         System.out.println("Please enter za sentence");
          String Sentence = scan.nextLine();  //need to get all words
         String [] words = Sentence.split(" ");
 
-         for(String MyWords:words){
-       //      boolean isPalindrome = Reverse();
-
+         for(String MyWord:words){
+            boolean isPalindrome = Reverse(MyWord);
+             System.out.println("is" +MyWord+"Palindrome = " + isPalindrome);
          }
-    }
 
+    }
 
     public static boolean Reverse(String word) {
         String ReverseString="";
         for (int i = 0; i < word.length(); i++) {
-            ReverseString = ReverseString+ word.charAt(i);
+            ReverseString =  word.charAt(i)+ReverseString;
         }
 
             return ReverseString.equalsIgnoreCase(word);
