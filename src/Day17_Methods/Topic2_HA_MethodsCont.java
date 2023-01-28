567c57ce83994fab4796e02aacfa969930699a5b
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-27 19:58:07 -0500

    Day17

---
+++ src/Day17_Methods/Topic2_HA_MethodsCont.java
@@ -0,0 +1,41 @@
+package Day17_Methods;
+
+public class Topic2_HA_MethodsCont {
+    public static void main(String[] args) {
+        // Write a method that makes food
+        // The method should take 3 String parameters which is Veggies, Salt and Oil
+        // You should concat them and print it.
+
+        /*
+        I need to create a method
+        this method should take 3 String parameters and parameters names Veggies, Salt and Oil
+        Return type void
+         */
+
+        Kitchen("Tomatoes","SeaSalt","OliveOil");
+
+    }
+
+    /// create a method
+    /// 3 String parameters. Veggies, Salt and Oil
+    /// No return, void
+    /// MethodName Kitchen
+    //public static ReturnType MethodName (Param1Type Param1Name, Param2Type Param2Name.......)
+
+    public static void Kitchen (String Veggies, String Salt, String Oil){
+        //You should concat them and print it.
+        String Food = Veggies+Salt+Oil;
+
+        String VeggieAndSalt = Veggies.concat(Salt);
+        String VeggieSaltAndOil = VeggieAndSalt.concat(Oil);
+        
+        String Fancy = Veggies.concat(Salt).concat(Oil);
+
+        System.out.println("Food = " + Food);
+        System.out.println("VeggieSaltAndOil = " + VeggieSaltAndOil);
+        System.out.println("Fancy = " + Fancy);
+    }
+
+
+
+}
