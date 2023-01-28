d170d5c2da72c3964ab5ae55a3a5ff0caa786c1d
GunelB <gunel.veli77@gmail.com>
2023-01-27 19:59:33 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day17_Methods/Topic2_GB_MethodsCont.java
@@ -0,0 +1,14 @@
+package Day17_Methods;
+
+public class Topic2_GB_MethodsCont {
+    public static void main(String[] args) {
+
+    }
+
+    public static void Food(String Veggies,String Salt,String Oil){
+        String VeggiesAndSalt = Veggies.concat(Salt);
+        String VeggiesSaltAndOil=Veggies.concat(Oil);
+        System.out.println("VeggiesSaltAndOil = " + VeggiesSaltAndOil);
+        System.out.println("VeggiesSaltAndOil = " + VeggiesSaltAndOil);
+    }
+}
