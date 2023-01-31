0f91af056ac8124c572cec7ae81dbae33e95c8e5
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-30 20:56:45 -0500

    Day18

--- src/Day18_Methods/Topic3_HA_Example2.java
+++ src/Day18_Methods/Topic3_HA_Example2.java
@@ -3,6 +3,8 @@ package Day18_Methods;
 public class Topic3_HA_Example2 {
     public static void main(String[] args) {
 /*
+https://codingbat.com/java
+
 Given 2 arrays of ints, a and b, return true if they have the same first element
 or they have the same last element.
 for ex.
