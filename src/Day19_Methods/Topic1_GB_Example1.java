d76999f0dc373712998421bcbb407793bf533fec
GunelB <gunel.veli77@gmail.com>
2023-01-31 19:25:14 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day19_Methods/Topic1_GB_Example1.java
@@ -0,0 +1,22 @@
+package Day19_Methods;
+
+public class Topic1_GB_Example1 {
+    public static void main(String[] args) {
+        /*
+Write a program to shift every element of an array to circularly right. E.g.-
+INPUT : 1 2 3 4 5
+OUTPUT : 5 1 2 3 4
+ */
+     //I need to create array
+        //need to iterate,need to create another empty array to put my result in it
+ //iterate each element and put it in the correct position
+
+        //need array
+       int[]MyNumbers={1,2,3,4,5};
+
+        //another int arrray
+        int [] MyNewNumbers = new int[MyNumbers.length];
+
+        //iterate each element
+    }
+}
