03edf3c3976af08c00f53d3fb83a9c73899ac63f
GunelB <gunel.veli77@gmail.com>
2023-01-31 21:47:07 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day19_Methods/Topic3_GB_Example2.java
@@ -0,0 +1,7 @@
+package Day19_Methods;
+
+public class Topic3_GB_Example2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Day19_Methods/Topic4_GB_Example3.java
@@ -0,0 +1,28 @@
+package Day19_Methods;
+
+public class Topic4_GB_Example3 {
+    public static void main(String[] args) {
+        /*
+Create a method which simulates the game "rock, paper, scissors". The function takes the input of both players (rock, paper or scissors), first parameter from first player, second from second player. The function returns the result as such:
+Rock beats scissors, paper beats rock, scissors beat paper.
+
+Examples:
+
+rps("rock", "paper") ➞ "Player 2 wins"
+
+rps("paper", "rock") ➞ "Player 1 wins"
+
+rps("paper", "scissors") ➞ "Player 2 wins"
+
+rps("scissors", "scissors") ➞ "TIE"
+
+rps("scissors", "paper") ➞ "Player 1 wins"
+ */
+        //create a method
+        //need  2 string parameters and return type String
+    }
+    public static String rps(String First,String Second){
+
+        return First;
+    }
+}
