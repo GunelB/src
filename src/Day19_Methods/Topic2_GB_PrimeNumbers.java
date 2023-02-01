3c39e9e9bca84fec30452e1bbdd2774d405b2033
GunelB <gunel.veli77@gmail.com>
2023-01-31 20:10:32 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day19_Methods/Topic2_GB_PrimeNumbers.java
@@ -0,0 +1,17 @@
+package Day19_Methods;
+
+public class Topic2_GB_PrimeNumbers {
+    public static void main(String[] args) {
+        /*
+Given two numbers  a and b as interval range, the task is to find the prime numbers in between this interval.
+
+Examples:
+
+Input : a = 1, b = 10
+Output : 2, 3, 5, 7
+
+Input : a = 10, b = 20
+Output : 11, 13, 17, 19
+ */
+    }
+}
