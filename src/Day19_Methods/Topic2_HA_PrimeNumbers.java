26a415f2edf1439bde0e474be53a8f35dbf848ba
Hayri Aslan <hayriaslan88@gmail.com>
2023-01-31 19:50:04 -0500

    Day19

---
+++ src/Day19_Methods/Topic2_HA_PrimeNumbers.java
@@ -0,0 +1,56 @@
+package Day19_Methods;
+
+import java.util.Scanner;
+
+public class Topic2_HA_PrimeNumbers {
+    public static void main(String[] args) {
+/*
+Given two numbers  a and b as interval range, the task is to find the prime numbers in between this interval.
+
+Examples:
+
+Input : a = 1, b = 10
+Output : 2, 3, 5, 7
+
+1 - no
+2 - yes, so print it
+3 - yes, so print it
+4 - no
+5 - yes, so print it
+6 - no
+7 - yes, so print it
+8 - no
+9 - no
+10 - no
+
+Input : a = 10, b = 20
+Output : 11, 13, 17, 19
+ */
+
+
+        /*
+        I need to get 2 numbers from user
+        start from a up to b, check if it is prime
+         */
+
+        //I need to get 2 numbers from user
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a : ");
+        int a = scan.nextInt();
+
+        System.out.println("Please enter b : ");
+        int b = scan.nextInt();
+
+        //start from a up to b, check if it is prime
+        for (int i = a; i <= b; i++) {
+            // i need to check if it is prime. If it is prime, i will print it
+            // Create a method to check if it is prime
+            // Parameters 1 parameter, int
+            // ReturnType boolean
+        }
+    }
+
+    public static boolean CheckPrimeNumber (int Number){
+        // i need to check if it is prime. If yes, return true, if not return false
+    }
+}
