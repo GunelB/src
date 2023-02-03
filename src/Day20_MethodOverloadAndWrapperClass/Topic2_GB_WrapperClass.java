2d9ed13d43a31e9dfcc2a5cf34e1ee0feffb11e6
GunelB <gunel.veli77@gmail.com>
2023-02-02 20:25:50 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day20_MethodOverloadAndWrapperClass/Topic2_GB_WrapperClass.java
@@ -0,0 +1,9 @@
+package Day20_MethodOverloadAndWrapperClass;
+
+public class Topic2_GB_WrapperClass {
+    public static void main(String[] args) {
+
+     //   ArrayList<Integer>MyArrayList=new ArrayList<int>();
+    }
+
+}
