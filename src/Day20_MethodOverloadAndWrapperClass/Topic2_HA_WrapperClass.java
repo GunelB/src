577a25f3ede019a5de3cdbf692f33328abefe7a6
Hayri Aslan <hayriaslan88@gmail.com>
2023-02-02 20:25:32 -0500

    Day20

---
+++ src/Day20_MethodOverloadAndWrapperClass/Topic2_HA_WrapperClass.java
@@ -0,0 +1,34 @@
+package Day20_MethodOverloadAndWrapperClass;
+
+import java.util.ArrayList;
+
+public class Topic2_HA_WrapperClass {
+    public static void main(String[] args) {
+        int k = 5;
+        double d = 5.5;
+
+        int [] intarray = {5,4};
+
+        //ArrayList
+
+//        int <- primitive
+//        Integer <- wrapper class
+
+        Integer wrapped_k = 5;
+        Double wrapped_d = 5.5;
+        String s = "ss";
+
+        boolean b = true;
+        Boolean wrapped_b = b;
+
+        int i = 5;
+        Integer wrapped_i = i;
+//        wrapped_i.toString()
+
+        //collection framework
+        //maps
+       // ArrayList<Integer> MyArrayList = new ArrayList<Integer>();
+
+        // Object oriented programming
+    }
+}
