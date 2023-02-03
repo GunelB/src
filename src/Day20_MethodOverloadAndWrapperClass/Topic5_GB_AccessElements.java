33ee8f6b2dc642ae3594e2f360ead55ecbf81bcd
GunelB <gunel.veli77@gmail.com>
2023-02-02 21:32:31 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day20_MethodOverloadAndWrapperClass/Topic5_GB_AccessElements.java
@@ -0,0 +1,20 @@
+package Day20_MethodOverloadAndWrapperClass;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Topic5_GB_AccessElements {
+    public static void main(String[] args) {
+        ArrayList<String>RandomWords =new ArrayList<>(2);
+
+        int i=0;
+        while(i<RandomWords.size());
+        System.out.println();
+        i++;
+        for(String word : RandomWords ){
+    }
+
+
+
+    }
+}
