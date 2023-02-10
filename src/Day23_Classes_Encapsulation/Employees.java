8f8175dbfe4b24d60fdde53779d156660fb2acd5
GunelB <gunel.veli77@gmail.com>
2023-02-09 19:52:38 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day23_Classes_Encapsulation/Employees.java
@@ -0,0 +1,9 @@
+package Day23_Classes_Encapsulation;
+
+public class Employees {
+    //Fields
+    String Name;
+    String Surname;
+    String Department;
+
+}
---
+++ src/Day23_Classes_Encapsulation/Topic1_GB_Classes.java
@@ -0,0 +1,16 @@
+package Day23_Classes_Encapsulation;
+
+public class Topic1_GB_Classes {
+    public static void main(String[] args) {
+        //DataType VariableName =new DataType()
+        Employees MyEmployee = new Employees();
+        MyEmployee.Name= "Gunel";
+        MyEmployee.Surname="Bagh";
+        MyEmployee.Department= "AAA";
+    }
+}
+//Encapsulation
+//public
+//private
+//protected
+//default - we can access from same package
