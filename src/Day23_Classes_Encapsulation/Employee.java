6e0ba092fc9b3f10e99f31c285e5f6b0bc3488b7
Hayri Aslan <hayriaslan88@gmail.com>
2023-02-09 19:52:20 -0500

    Day23

--- src/Day23_Classes_Encapsulation/Employee.java
+++ src/Day23_Classes_Encapsulation/Employee.java
@@ -8,6 +8,23 @@ public class Employee {
     String Surname;
     String Department;
 
+    // 9 digits
+    private String SIN;
+
+    // Getters and Setters
+    void SetSIN(String p_SIN){
+        if (p_SIN.length() == 9){
+            SIN = p_SIN;
+        }
+    }
+
+    String GetSIN(){
+        // I would like to get last 3 digits of SIN
+        //856458745   Length = 9
+        //012345678
+        return "******"+ SIN.substring(SIN.length()-3);
+    }
+
     // Methods
     // Get Full Name
     // Create a method, which returns full name
--- src/Day23_Classes_Encapsulation/Topic1_HA_Classes.java
+++ src/Day23_Classes_Encapsulation/Topic1_HA_Classes.java
@@ -4,11 +4,15 @@ public class Topic1_HA_Classes {
     public static void main(String[] args) {
         // DataType VariableName = new Datatype()
         Employee MyEmployee = new Employee();
-        MyEmployee.Name = "Hayri";
+        MyEmployee.Name = "Henry";
         MyEmployee.Surname = "Aslan";
         MyEmployee.Department = "R&D";
 
+        MyEmployee.SetSIN("123456789");
+
         System.out.println(MyEmployee.GetFullName());
+
+        System.out.println(MyEmployee.GetSIN());
 
     }
 }
