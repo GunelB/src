c54030ca48e2a164f0d2728d453768c783b7b9ab
Hayri Aslan <hayriaslan88@gmail.com>
2023-02-13 21:20:46 -0500

    Day25

---
+++ src/Day25_ClassesandInheritance/Dog.java
@@ -0,0 +1,12 @@
+package Day25_ClassesandInheritance;
+
+public class Dog extends OldMcDonaldAnimal{
+    public void Dig(){
+
+    }
+
+    @Override
+    public void Sound() {
+        System.out.println("Woof Woof");
+    }
+}
---
+++ src/Day25_ClassesandInheritance/OldMcDonaldAnimal.java
@@ -0,0 +1,16 @@
+package Day25_ClassesandInheritance;
+
+public class OldMcDonaldAnimal {
+    String Name;
+    String Color;
+    String Tag;
+    int NumberOfLegs;
+
+    public void Eat(){
+        System.out.println(Name+" is eating!");
+    }
+
+    public void Sound (){
+        System.out.println("Default Sound Method");
+    }
+}
