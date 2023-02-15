5905edf00377688c1eeaa358a36aa382f4528e6c
GunelB <gunel.veli77@gmail.com>
2023-02-14 21:48:22 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day26_InheritanceandPolyMorphism/Topic3_GB_PolyMorphism.java
@@ -0,0 +1,16 @@
+package Day26_InheritanceandPolyMorphism;
+
+public class Topic3_GB_PolyMorphism {
+    public static void main(String[] args) {
+        HourlyStaff_GB Staff1 =new HourlyStaff_GB();
+        Staff1.HoursWorked = 35;
+        Staff1.PayRate = 60;
+
+        HourlyStaff_GB Staff2 = new HourlyStaff_GB();
+        Staff2.HoursWorked = 20;
+        Staff2.PayRate = 40;
+        GB_SalaryClass Staff3
+
+
+    }
+}
