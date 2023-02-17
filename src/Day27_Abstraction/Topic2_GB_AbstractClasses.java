2178e1d659f367da242c0fccce3ef94f43af8b35
GunelB <gunel.veli77@gmail.com>
2023-02-16 20:42:05 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

---
+++ src/Day27_Abstraction/Topic2_GB_AbstractClasses.java
@@ -0,0 +1,18 @@
+package Day27_Abstraction;
+
+public class Topic2_GB_AbstractClasses {
+    public static void main(String[] args) {
+
+    }
+}
+abstract class Bakery_GB {
+    public void AMethod(){
+
+}
+
+interface AnInterface {
+
+   void AnAbstractMethod();
+
+    }
+}
