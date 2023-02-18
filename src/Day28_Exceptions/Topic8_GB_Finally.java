874fc04dc95dbb2a2b32ffad023230a91083c485
GunelB <gunel.veli77@gmail.com>
2023-02-18 00:10:21 -0500

    Merge branch 'master' of https://github.com/renastechschool/RenastechJava8

--- src/Day28_Exceptions/Topic8_GB_Finally.java
+++ src/Day28_Exceptions/Topic8_GB_Finally.java
@@ -17,3 +17,61 @@ public class Topic8_GB_Finally {
         }
     }
 }
+
+//package Day28_Exceptions;
+
+  //      import java.util.Scanner;
+
+//public class Topic8_HA_Finally {
+  //  public static void main(String[] args) {
+  //      Scanner scan = new Scanner(System.in);
+   //     int a = scan.nextInt();
+    //    try {
+            //your code will go here
+          //  int value = 10 / a;
+   ////         System.out.println("value = " + value);
+    //    } catch (ArithmeticException e){
+    //        System.out.println("There is an exception!");
+      //  } finally {
+        //    System.out.println("This code will get executed no matter what");
+      //  }
+
+
+   //     try {
+    //        System.out.println("Imagine you are opening a file here");
+
+            /*
+            do something with file
+             */
+      //  } finally {
+        //    System.out.println("Imagine you are closing the file");
+       // }
+
+
+
+
+/*
+try {}
+catch(){}
+catch(){}
+finally{}
+try {}
+catch(){}
+catch(){}
+try{}
+finally{}
+Below combinations are wrong
+try{
+}
+sout()
+catch(){
+}
+catch(){}
+finally{}
+catch(){}
+try{}
+try{}
+finally{}
+finally{}
+try{}
+ */
