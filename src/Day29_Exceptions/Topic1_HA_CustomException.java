bc7ee76fcb7acd2955bb4b43796561052146662d
Hayri Aslan <hayriaslan88@gmail.com>
2023-02-21 19:34:29 -0500

    Day29

---
+++ src/Day29_Exceptions/Topic1_HA_CustomException.java
@@ -0,0 +1,39 @@
+package Day29_Exceptions;
+
+public class Topic1_HA_CustomException {
+    public static void main(String[] args) {
+        try {
+            ValidateAge(15);
+        }catch (NotEligibleToVoteException e){
+            System.out.println(e.Message);
+        }
+    }
+
+    /*
+
+    try
+    catch
+    finally
+    throw
+    throws
+     */
+
+    public static void ValidateAge(int p_Age) throws NotEligibleToVoteException {
+        if (p_Age < 18){
+            throw new NotEligibleToVoteException("You can not vote!");
+        }
+        else {
+            System.out.println("You can vote");
+        }
+    }
+}
+
+class NotEligibleToVoteException extends Exception{
+    String Message;
+
+    // create a constructor for this class that takes a string param and assign it to message
+    public NotEligibleToVoteException(String p_Message){
+        Message = p_Message;
+    }
+}
+
