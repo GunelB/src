0720dc9344f42719c0bab2bcb62cb2eb182a3280
Hayri Aslan <hayriaslan88@gmail.com>
2023-03-02 19:39:44 -0500

    Day34

--- src/Day34_FileOperations/Topic2_HA_FileWriter.java
+++ src/Day34_FileOperations/Topic2_HA_FileWriter.java
@@ -1,13 +1,16 @@
 package Day34_FileOperations;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Topic2_HA_FileWriter {
     public static void main(String[] args) {
         try {
-            FileWriter FW = new FileWriter("NewFile_HA2.txt");
+            FileWriter FW = new FileWriter("NewFile_HA3.txt");
             FW.write("Hello, this is my first line.");
+            FW.write(13); // 13 means Return key (ENTER KEY)
             FW.write("Hi Again!");
 
             FW.close();
