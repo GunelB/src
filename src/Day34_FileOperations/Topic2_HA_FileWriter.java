c2686d1a0cf3467a31aa0fbc5570fa7c6e98a5c7
Hayri Aslan <hayriaslan88@gmail.com>
2023-03-02 19:35:18 -0500

    Day34

--- src/Day34_FileOperations/Topic2_HA_FileWriter.java
+++ src/Day34_FileOperations/Topic2_HA_FileWriter.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Topic2_HA_FileWriter {
     public static void main(String[] args) {
         try {
-            FileWriter FW = new FileWriter("NewFile_HA.txt");
+            FileWriter FW = new FileWriter("NewFile_HA2.txt");
             FW.write("Hello, this is my first line.");
             FW.write("Hi Again!");
 
