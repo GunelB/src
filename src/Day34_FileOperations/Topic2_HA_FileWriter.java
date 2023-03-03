080549b231e6a64eb0a2d268f2c89b031e077f51
Hayri Aslan <hayriaslan88@gmail.com>
2023-03-02 20:48:33 -0500

    Day34

--- src/Day34_FileOperations/Topic2_HA_FileWriter.java
+++ src/Day34_FileOperations/Topic2_HA_FileWriter.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Topic2_HA_FileWriter {
     public static void main(String[] args) {
         try {
-            FileWriter FW = new FileWriter("NewFile_HA3.txt");
+            FileWriter FW = new FileWriter("NewFile_HA5.txt");
             FW.write("Hello, this is my first line.");
             FW.write(13); // 13 means Return key (ENTER KEY)
 //            FW.write("\n");  <- this line will also add a new line
---
+++ src/Day34_FileOperations/Topic3_HA_Example.java
@@ -0,0 +1,65 @@
+package Day34_FileOperations;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Topic3_HA_Example {
+    public static void main(String[] args) {
+        try {
+            FileReader FR = new FileReader("top250.txt");
+            BufferedReader MyReader = new BufferedReader(FR);
+
+            ArrayList<Movie> Top250 = new ArrayList<>();
+
+            String line = MyReader.readLine();
+
+            while (line != null){
+                // do something
+                //line = 9.2  The Shawshank Redemption (1994)
+                StringBuilder SB = new StringBuilder(line);
+
+                String R = SB.substring(0,3);  // 9.2
+                System.out.println(R);
+
+                //line = 9.2  The Shawshank Redemption (1994)
+                SB.delete(0,5);
+
+                //line = The Shawshank Redemption (1994)
+                int Position1 = SB.indexOf("(");
+                String Name = SB.substring(0, Position1).trim();
+
+                System.out.println(Name);
+                //line = The Shawshank Redemption (1994)
+                SB.delete(0, Position1+1);
+
+                //line = 1994)
+                String Y = SB.substring(0,4);
+                System.out.println(Y);
+
+                Movie AMovie = new Movie();
+                AMovie.MovieName = Name;
+                AMovie.Year = Integer.parseInt(Y);
+                AMovie.Rating = Double.parseDouble(R);
+                Top250.add(AMovie);
+
+                line = MyReader.readLine();
+            }
+
+
+
+        }catch (FileNotFoundException e){
+            System.out.println("top250.txt not found!");
+        }catch (IOException e){
+            System.out.println("Something happened while reading the file!");
+        }
+    }
+}
+
+class Movie{
+    String MovieName;
+    int Year;
+    double Rating;
+}
