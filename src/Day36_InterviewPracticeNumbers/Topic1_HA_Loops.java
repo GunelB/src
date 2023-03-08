5cb973686db3a82ba84fa10b3bc54a3545d2376a
Hayri Aslan <hayriaslan88@gmail.com>
2023-03-07 19:13:24 -0500

    Day36

---
+++ src/Day36_InterviewPracticeNumbers/Topic1_HA_Loops.java
@@ -0,0 +1,28 @@
+package Day36_InterviewPracticeNumbers;
+
+public class Topic1_HA_Loops {
+    public static void main(String[] args) {
+        String[] Words = {"Java", "Derya", "Starbucks", "Ticketmaster", "Tesla"};
+
+        System.out.println("***Using For Loop***");
+
+        for(int i=0; i < Words.length; i++){
+            System.out.println(Words[i]);
+        }
+
+        System.out.println("***Using For Each***");
+        for(String MyWord : Words){
+            System.out.println(MyWord);
+        }
+
+        System.out.println("***Using While Loop***");
+        int i = 0;
+        while (i < Words.length){
+            System.out.println(Words[i]);
+            i++;
+        }
+
+
+
+    }
+}
