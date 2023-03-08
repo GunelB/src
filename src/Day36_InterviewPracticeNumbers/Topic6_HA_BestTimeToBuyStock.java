3410a31f3279ae98a98b23e85bfb3a8f81fcc5bc
Hayri Aslan <hayriaslan88@gmail.com>
2023-03-07 21:35:38 -0500

    Day36

---
+++ src/Day36_InterviewPracticeNumbers/Topic6_HA_BestTimeToBuyStock.java
@@ -0,0 +1,54 @@
+package Day36_InterviewPracticeNumbers;
+
+public class Topic6_HA_BestTimeToBuyStock {
+    public static void main(String[] args) {
+        // prices {7,1,5,3,6,4}
+
+        // print maximum profit
+        // buy it when it is in the lowest price
+        // sell it when it is in the highest price
+
+        /*
+        Day 1 Selling Price = 7
+        Day 2 Selling Price = 1
+        Day 3 Selling Price = 5
+        Day 4 Selling Price = 3
+        Day 5 Selling Price = 6
+        Day 6 Selling Price = 4
+
+        imagine you bought it when it was 1
+        if we sell it when it is 5, the profit will be = 4;
+        if we sell it when it is 3, the profit will be = 2;
+        if we sell it when it is 6, the profit will be = 5;   <-- Max Profit
+        if we sell it when it is 4, the profit will be = 3;
+
+        You can not buy and sell same day
+         */
+
+        int[] data = {7,1,5,3,6,4};
+
+        //find the lowest value to buy
+
+        int BuyingPrice = data[0];
+        int Profit = 0;
+        for (int i = 1; i < data.length; i++) {
+            // if we find the lowest value to buy, assign buying price to it
+            if (BuyingPrice > data[i]){
+                BuyingPrice = data[i];
+            } else {
+                // selling it
+                // I need to find what profit I will make if I sell it here
+                // Selling price = 5
+                int NewProfit = data[i] - BuyingPrice;  // NewProfit = 4
+
+                if (NewProfit > Profit){
+                    Profit = NewProfit;
+                }
+            }
+        }
+
+        System.out.println("Buying Price = "+BuyingPrice);
+        System.out.println("Profit = "+Profit);
+
+    }
+}
