e8d50fa703aa2f4261a941a49feec35797f3f23d
Hayri Aslan <hayriaslan88@gmail.com>
2023-03-10 19:38:54 -0500

    Day38

--- src/Day38_InterviewPractice/Topic2_HA_Fibonacci2.java
+++ src/Day38_InterviewPractice/Topic2_HA_Fibonacci2.java
@@ -1,17 +1,29 @@
 package Day38_InterviewPractice;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Topic2_HA_Fibonacci2 {
     public static void main(String[] args) {
         //if we want to print fibonacci numbers between 89 - 10946
+
+        ArrayList<Integer> Numbers = new ArrayList<>();
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the minimum value");
+        int min = scan.nextInt();
+
+        System.out.println("Please enter the maximum value");
+        int max = scan.nextInt();
 
         int a = 0;
         int b = 1;
 
         int Nextone = a + b;
 
-        while(Nextone <= 10946){
-            if (Nextone >= 89){
-                System.out.print(Nextone+" ");
+        while(Nextone <= max){
+            if (Nextone >= min){
+                Numbers.add(Nextone);
             }
 
             a = b;
@@ -19,5 +31,7 @@ public class Topic2_HA_Fibonacci2 {
             Nextone = a + b;
 
         }
+
+        System.out.println(Numbers);
     }
 }
