bcdec0ea67638cff6d4c7bbd4eb49590e8357716
Hayri Aslan <hayriaslan88@gmail.com>
2023-03-10 20:44:40 -0500

    Day38

--- src/Day38_InterviewPractice/Topic6_HA_ReverseNumber.java
+++ src/Day38_InterviewPractice/Topic6_HA_ReverseNumber.java
@@ -11,10 +11,13 @@ public class Topic6_HA_ReverseNumber {
         System.out.println("Please enter a number");
         Integer Number = scan.nextInt();
 
+        //159     159%10  = 9
+
+
         int Reversed = 0;
         while(Number != 0){
             // 1 5 9
-            int remainder = Number % 10;   // 159%10 = 9
+            int remainder = Number % 10;   // 159%10 = 9  // 9        0 * 10 + 9
             Reversed = Reversed * 10 + remainder;         // 95       9 * 10 + 5
                                                           // 951      95 * 10 + 1
             // 1 5 9
